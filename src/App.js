4a327ff23732cceb966be0e3e0d63e80fa09bbe0
EGalinevsky <e.galinevsky@gmail.com>
2021-12-06 12:43:30 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React, { useState } from 'react'
 import Navbar from './Navbar';
+import NavItem from './NavItem';
+import { ReactComponent as PlusIcon } from './icon/plus.svg'
+import { ReactComponent as BellIcon } from './icon/bell.svg'
+import { ReactComponent as MessengerIcon } from './icon/messenger.svg'
+import { ReactComponent as CaretIcon } from './icon/caret.svg'
+import DropDownMenu from './DropDownMenu';
 
 function App() {
   return (
     <>
       <Navbar>
-        <li>x</li>
+        <NavItem icon={<PlusIcon />} />
+        <NavItem icon={<BellIcon />} />
+        <NavItem icon={<MessengerIcon />} />
+
+        <NavItem icon={<CaretIcon />}>
+          <DropDownMenu />
+          <p>
+            Hello
+          </p>
+
+        </NavItem>
       </Navbar>
     </>
   );
