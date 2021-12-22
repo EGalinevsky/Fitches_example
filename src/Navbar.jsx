ef22bcd1c79565d0abbe458e2fb968ff14df15c4
EGalinevsky <e.galinevsky@gmail.com>
2021-12-22 10:01:54 +0000

    re1

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Navbar = (props) => {
     return (
         <nav className="navbar">
+            1
             <ul className="navbar-nav">
                 {props.children}
             </ul>
